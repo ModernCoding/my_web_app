4f1a8da8c00f7858704fbac7e04d76f15864ce87
Modern Coding <cormorant.programming@gmail.com>
2019-03-07 18:36:58 +0700

    Preparing responsive design course

--- scripts/jquery/index.js
+++ scripts/jquery/index.js
@@ -1,64 +1,6 @@
 // $(window).resize(logScreenSize);
 
-
 $(function(){
   // logScreenSize();
   resizeElements(['.my-button-people-name', '.my-button-jump-to-people'])
 });
-
-
-function logScreenSize(){
-
- var size;
-
-  switch (true) {
-
-    case Modernizr.mq('(min-width: 1200px)'):
-      size = 'xl';
-      break;
-
-    case Modernizr.mq('(min-width: 992px)'):
-      size = 'lg';
-      break;
-
-    case Modernizr.mq('(min-width: 768px)'):
-      size = 'md';
-      break;
-
-    case Modernizr.mq('(min-width: 576px)'):
-      size = 'sm';
-      break;
-
-    default:
-      size = 'xs';
-      break;
-
-  }
-
-  console.log(`The size of your screen is ${size}`);
-
-}
-
-
-function resizeElements(elements){
-
-  $.each(elements, function(index, element){
-    var width = 0;
-
-    $(element).css('width', 'auto');
-
-    $(element).each(function(){
-
-      var thisWidth = parseFloat($(this).css('width'));
-
-      if(thisWidth > width) {
-        width = thisWidth;
-      }
-
-    });
-
-    $(element).css('width', width);
-
-  });
-
-}
